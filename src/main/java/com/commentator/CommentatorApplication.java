851863ab9dc7d06472775ca3c525bb82d2193127
attackTheChocha <conor-henry@uiowa.edu>
2021-08-14 21:42:12 -0500

    First commit

---
+++ src/main/java/com/commentator/CommentatorApplication.java
@@ -0,0 +1,13 @@
+package com.commentator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/commentator/CommentatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commentator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
